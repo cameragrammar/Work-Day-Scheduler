0c75a3b4f74dd8642b3efc209da6d593a7f61b39
Hans Shen <116024825+cameragrammar@users.noreply.github.com>
2022-12-12 14:17:50 -0700

    Upload new JS file

--- Develop/script.js
+++ Develop/script.js
@@ -1,49 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-tasks = [];
-
-
-var loadTasks= function (){
-    tasks = JSON.parse(localStorage.getItem("tasks"))
-    if(!tasks) {
-        tasks={};
-    } ;
-    printTasks(tasks)
-  }
-
-  var printTasks = function(){
-    $.each(tasks, function(list, arr){
-
-      var taskP = $("<p>").addClass("description task-item-" + list).text(arr)
-
-      $("#task-item-" + list).replaceWith(taskP);
-    })
-  }
-
-
-var Today = (moment().format("MMMM D, YYYY"))
-  $("#currentDay").text(Today);
-
-  //color code hours bins
-
-
-var hourAudit =function(){
-  var currentHour = moment().hour()
-
-  for(var i=8; i<18; i++){
-    var taskArea = $("#task-"+i)
-    if(currentHour>i){
-      $(taskArea).addClass("past");
-    } else if (currentHour === i){
-        $(taskArea).addClass("present");
-    }else{
-        $(taskArea).addClass("future")
-    }
-  }
-}
-
-
+$(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -51,56 +9,66 @@ var hourAudit =function(){
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
 
-$(".taskBin").on("click", "p", function(){
-    var text = $(this)
-      .text()
-      .trim();
-
-    var textInput = $("<textarea>")
-      .addClass("form-control")
-      .val(text);  
-
-      $(this).replaceWith(textInput);
-      textInput.trigger("focus");
-});
-
-$(".taskBin").on("blur", "textarea", function(){
-
-    var text = $(this)
-      .val()
-      .trim();
-
-    var taskP = $("<p>")
-      .addClass("taskItem")
-      .text(text);
-      
-    $(this).replaceWith(taskP);
-});
-
 $(".saveBtn").on("click", function(){
-    
-    var index = $(".saveBtn").index(this);
+  var text = $(this).siblings(".description").val();
+  var hour = $(this).parent().attr("id").substring(5);
+  localStorage.setItem(hour, text);
 
-    tasks[index] = $(this).parent().find(".taskItem").text();
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-});
+})
 
-
-setInterval(function(){
-  hourAudit();},1000*60*60);
-
-  loadTasks();
-  hourAudit();
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+  function updateTime(){
+    var currentHour = dayjs().hour();
+    $(".time-block").each(function(){
+      var blockHour = Number($(this).attr("id").substring(5));
+      if(blockHour < currentHour){
+        $(this).addClass("past");
+        $(this).removeClass("present");
+        $(this).removeClass("future");
+      }
+      else if(blockHour == currentHour){
+        $(this).addClass("present");
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+      }
+      else{
+        $(this).addClass("future");
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+      }
+    })
+  } 
+
+  updateTime();
+  setInterval(updateTime, 60000);
+
+
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+
+for(var i = 9; i <= 17; i++){
+  var text = localStorage.getItem(i);
+  $("#hour-" + i + " .description").val(text);
+  
+}
+
+
   //
   // TODO: Add code to display the current date in the header of the page.
+  //const dayjs = require('dayjs');
+  //import dayjs from 'dayjs' // ES 2015
+  var today = dayjs().format("dddd, MMMM D");
+  $("#currentDay").text(today);
 
+  
+  
+
+
+});
